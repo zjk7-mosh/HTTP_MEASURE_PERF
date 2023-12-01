140ed0bc824ed9f32eac8431a79b327e30f11b9d
zjk7-mosh <145497712+zjk7-mosh@users.noreply.github.com>
2023-12-01 18:53:10 -0500

    Initial commit 	new file:   HTTP_MEASURE_PERF.code-workspace 	new file:   example-scapy.py 	new file:   measure-webserver.py 	new file:   pcap1.pcap 	new file:   pcap2.pcap 	new file:   pcap3.pcap

---
+++ measure-webserver.py
@@ -0,0 +1,12 @@
+# Input Arguments: a pcapng file, a destination server IP address and a destination port, as:
+# measure-webserver.py <input-file> <server-ip> <server-port>
+
+# The code must produce 2 lines of output:
+# AVERAGE LATENCY: <float>
+# PERCENTILES: <float-25%> , <float-50%>, <float-75%>, <float-95%>, <float-99%>
+
+# Note: values are to be reported in seconds. 
+# Use at least 5 digits of accuracy to the left of the decimal point. 
+# The autograder will give a +/- 5% tolerance on the answer
+
+
